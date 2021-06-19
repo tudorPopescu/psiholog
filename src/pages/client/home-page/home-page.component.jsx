e9ade8192f7a1e5bb1fee65d50ac7e9961bcabbd
tudorPopescu <tudorp2@gmail.com>
2021-06-19 08:59:47 +0300

    delete src

--- src/pages/client/home-page/home-page.component.jsx
+++
@@ -1,7 +0,0 @@
-const ClientHomePage = () => (
-  <div>
-    Client Home Page
-  </div>
-);
-
-export default ClientHomePage;
--- src/redux/user/user.actions.js
+++
@@ -1,8 +0,0 @@
-import { userTypes } from './user.types';
-
-export const setCurrentUser = user => {
-  return ({
-    type: userTypes.SET_CURRENT_USER,
-    payload: user
-  });
-};
